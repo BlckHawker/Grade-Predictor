49a3e31e1727803f57b41f25ee48669996fee7ef
Hawker <kovujacksonbentley@gmail.com>
2025-04-09 07:09:17 -0400

    Attendance Apr 7 - 9

--- src/component/LastUpdate.tsx
+++ src/component/LastUpdate.tsx
@@ -1,7 +1,7 @@
 import data from '../data.json'
 function LastUpdate() {
     let date = new Date(data["LastUpdate"]);
-    return <p>Website Last update {`${date.toLocaleDateString()} ${date.getHours()}:${date.getMinutes()}`}</p>
+    return <p>Website Last update {`${date.toLocaleDateString()} ${date.getHours()}:${date.getMinutes().toString().padStart(2, "0")}`}</p>
   }
   
   export default LastUpdate;
